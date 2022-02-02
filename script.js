2804134b2a01bac51229e096351896020e6fca42
bernix47 <selvasekarbernix@gmail.com>
2022-02-02 22:19:32 +0530

    Added all Dependent Files

---
+++ script.js
@@ -0,0 +1,29 @@
+const sliders = document.querySelectorAll("input[type='range']");
+/* console.log(sliders); */
+sliders.forEach(function(slider){
+    slider.addEventListener('input',calculateTip)
+});
+
+const billInput = document.getElementById("bill");
+billInput.addEventListener('change',calculateTip);
+
+function calculateTip(){
+    let bill = parseFloat(billInput.value);
+    let tipPercent = parseFloat(document.getElementById("tip").value);
+    let noOfPeople = parseFloat(document.getElementById("no-of-people").value);
+
+    billInput.value = bill.toFixed(2);
+
+    let totalTip = parseFloat((bill * (tipPercent/100)).toFixed(2));
+    let total = parseFloat((bill + totalTip).toFixed(2));
+    let tipPerPerson = Math.ceil((totalTip / noOfPeople).toFixed(2));
+    let totalPerPerson = Math.ceil((total / noOfPeople).toFixed(2));
+    console.log(total,totalTip,tipPerPerson,totalPerPerson,bill,tipPercent,noOfPeople);
+
+    document.getElementById('tip-amount').textContent = `\₹${totalTip}`;
+    document.getElementById('total-amount').textContent = `\₹${total}`;
+    document.getElementById('tip-percent').textContent = tipPercent;
+    document.getElementById('split-num').textContent = noOfPeople;
+    document.getElementById('tip-per-person').textContent = `\₹${tipPerPerson}`;
+    document.getElementById('total-per-person').textContent = `\₹${totalPerPerson}`;
+}
